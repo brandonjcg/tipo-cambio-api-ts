85d11340ed818b48aa021bf05b1cc4189465aa2a
Brandon Castillo <brajoel98@gmail.com>
2023-05-16 21:37:07 -0700

    feat(models): :art: Fix eslint issues

--- models/server.ts
+++ models/server.ts
@@ -1,19 +1,20 @@
 import express from 'express';
 
 class Server {
-    private app: express.Application;
-    private port: string;
+  private app: express.Application;
 
-    constructor() {
-        this.app = express();
-        this.port = process.env.PORT || '9001';
-    }
+  private port: string;
 
-    listen () {
-        this.app.listen(this.port, () => {
-            console.log(`🟢 Server running on port ${this.port}`)
-        });
-    }
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || '9001';
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`🟢 Server running on port ${this.port}`);
+    });
+  }
 }
 
 export default Server;
