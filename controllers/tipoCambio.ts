a866b69e57ce384e904e814e517f4555c6e7d40d
Brandon Castillo <brajoel98@gmail.com>
2023-08-17 19:28:11 -0700

    feat(app): :sparkles: Create service to get exchange rate from DOF
    Endpoint return data in XML

--- controllers/tipoCambio.ts
+++ controllers/tipoCambio.ts
@@ -1,9 +1,14 @@
 import { Request, Response } from 'express';
+import getExchangeRateFromDof from '../services/http';
 
 export default async (req: Request, res: Response) => {
   try {
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = String(0);
+
+    const data = await getExchangeRateFromDof();
     return res.json({
       success: false,
+      data,
       message: 'Controller get tipo de cambio',
     });
   } catch (err: any) {
@@ -11,5 +16,7 @@ export default async (req: Request, res: Response) => {
       success: true,
       message: `${err.name}: ${err.message}`,
     });
+  } finally {
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = String(1);
   }
 };
