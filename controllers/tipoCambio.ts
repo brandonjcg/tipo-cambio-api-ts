6a87d91086e3ea0322bf2c9a2678236d777961e4
Brandon Castillo <brajoel98@gmail.com>
2023-08-17 21:47:31 -0700

    feat(controllers): :sparkles: Implement utils in controller to obtain exchange rate

--- controllers/tipoCambio.ts
+++ controllers/tipoCambio.ts
@@ -1,19 +1,26 @@
 import { Request, Response } from 'express';
+import getExchangeRateFromJson from '../helpers/exchangeRate';
+import parseXml2Json from '../helpers/parsers';
 import getExchangeRateFromDof from '../services/http';
 
 export default async (req: Request, res: Response) => {
   try {
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = String(0);
 
-    const data = await getExchangeRateFromDof();
+    const dataXml = await getExchangeRateFromDof();
+
+    const dataJson = parseXml2Json(dataXml);
+
+    const data = getExchangeRateFromJson(dataJson);
+
     return res.json({
-      success: false,
+      success: true,
       data,
       message: 'Controller get tipo de cambio',
     });
   } catch (err: any) {
     return res.status(500).json({
-      success: true,
+      success: false,
       message: `${err.name}: ${err.message}`,
     });
   } finally {
